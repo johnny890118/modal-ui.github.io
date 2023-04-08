d887b79942478671996741188aca1aae16d7631f
johnny890118 <128392036+johnny890118@users.noreply.github.com>
2023-04-09 03:56:27 +0800

    modal-ui

---
+++ modal-ui/script.js
@@ -0,0 +1,22 @@
+let loginmodal = document.querySelector("#login-modal");
+let postmodal = document.querySelector("#post-modal");
+
+function loginModal() {
+  let loginmodal = document.querySelector("#login-modal");
+  loginmodal.style.display = "block";
+}
+
+function closebtn() {
+  let loginmodal = document.querySelector("#login-modal");
+  loginmodal.style.display = "none";
+}
+
+function postModal() {
+  let postmodal = document.querySelector("#post-modal");
+  postmodal.style.display = "block";
+}
+
+function closebtn2() {
+  let postmodal = document.querySelector("#post-modal");
+  postmodal.style.display = "none";
+}
